3dcea773ad034653f61f806a966c29fcf6b15a70
Morten Nilsen <morten@runsafe.no>
2012-04-12 22:00:11 +0200

    Rework a bit, so as to not poison other plugins containers.

--- src/no/runsafe/RunsafeMessageBus.java
+++ src/no/runsafe/RunsafeMessageBus.java
@@ -1,12 +1,19 @@
 package no.runsafe;
 
+import no.runsafe.framework.RunsafePlugin;
+import no.runsafe.framework.messaging.IMessagePump;
 import no.runsafe.framework.messaging.MessagePump;
 
-public class RunsafeMessageBus extends MessagePump
+public class RunsafeMessageBus extends RunsafePlugin
 {
-    @Override
-    protected void PluginSetup()
-    {
-        //No code needed
-    }
+	@Override
+	protected void PluginSetup()
+	{
+		this.addComponent(new MessagePump());
+	}
+
+	public IMessagePump getInstance()
+	{
+		return getComponent(IMessagePump.class);
+	}
 }
