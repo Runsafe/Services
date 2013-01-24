0f10545ca36a3df24ee89a49f42d7cc3ae9c6eec
Morten Nilsen <morten@runsafe.no>
2013-01-24 18:58:24 +0100

    Remove deprecated API implementation

--- src/no/runsafe/RunsafeServices.java
+++ src/no/runsafe/RunsafeServices.java
@@ -8,20 +8,15 @@ import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.command.Command;
 import no.runsafe.framework.configuration.IConfiguration;
 import no.runsafe.framework.event.IPluginEnabled;
-import no.runsafe.framework.messaging.IMessagePump;
-import no.runsafe.framework.messaging.IPumpProvider;
-import no.runsafe.framework.messaging.MessagePump;
 import no.runsafe.framework.output.IOutput;
 
 import java.util.logging.Level;
 
-public class RunsafeServices extends RunsafeConfigurablePlugin implements IPumpProvider, IPluginEnabled
+public class RunsafeServices extends RunsafeConfigurablePlugin implements IPluginEnabled
 {
 	@Override
 	protected void PluginSetup()
 	{
-		this.addComponent(MessagePump.class);
-
 		Command command = new Command("runsafe", "Runsafe core services", null);
 
 		Command config = new Command("config", "Commands to control plugin configurations", null);
@@ -34,11 +29,6 @@ public class RunsafeServices extends RunsafeConfigurablePlugin implements IPumpP
 		addComponent(command);
 	}
 
-	public IMessagePump getInstance()
-	{
-		return getComponent(IMessagePump.class);
-	}
-
 	@Override
 	public void OnPluginEnabled()
 	{
@@ -46,8 +36,8 @@ public class RunsafeServices extends RunsafeConfigurablePlugin implements IPumpP
 		if (level != null)
 		{
 			Level debugLevel = Level.parse(level);
-			for (RunsafePlugin plugin : RunsafePlugin.Instances.values())
-				plugin.getComponent(IOutput.class).setDebugLevel(debugLevel);
+			for (IOutput console : RunsafePlugin.getPluginAPI(IOutput.class))
+				console.setDebugLevel(debugLevel);
 		}
 	}
 }
