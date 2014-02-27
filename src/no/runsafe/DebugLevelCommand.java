7399bf90efccf5e32693c72df8b920dd141d90da
Morten Nilsen <morten@runsafe.no>
2014-02-27 21:29:10 +0100

    Upstream changes

--- src/no/runsafe/DebugLevelCommand.java
+++ src/no/runsafe/DebugLevelCommand.java
@@ -3,14 +3,14 @@ package no.runsafe;
 import no.runsafe.framework.RunsafePlugin;
 import no.runsafe.framework.api.IKernel;
 import no.runsafe.framework.api.command.ICommandExecutor;
-import no.runsafe.framework.api.command.argument.EnumArgument;
+import no.runsafe.framework.api.command.argument.Enumeration;
+import no.runsafe.framework.api.command.argument.IArgumentList;
 import no.runsafe.framework.api.command.argument.RequiredArgument;
 import no.runsafe.framework.api.command.console.ConsoleCommand;
 import no.runsafe.framework.api.log.IDebug;
 import no.runsafe.framework.internal.log.Debug;
 
 import javax.annotation.Nonnull;
-import java.util.Map;
 import java.util.logging.Level;
 
 public class DebugLevelCommand extends ConsoleCommand
@@ -19,7 +19,7 @@ public class DebugLevelCommand extends ConsoleCommand
 	{
 		super(
 			"debuglevel", "Changes the output debug level for plugins",
-			new RequiredArgument("plugin"), new EnumArgument("level", DebugLevel.values(), true)
+			new RequiredArgument("plugin"), new Enumeration.Required("level", DebugLevel.values())
 		);
 	}
 
@@ -32,10 +32,10 @@ public class DebugLevelCommand extends ConsoleCommand
 
 	@SuppressWarnings("StringToUpperCaseOrToLowerCaseWithoutLocale")
 	@Override
-	public String OnExecute(Map<String, String> parameters)
+	public String OnExecute(IArgumentList parameters)
 	{
 		String pluginName = parameters.get("plugin");
-		Level level = Level.parse(parameters.get("level"));
+		Level level = parameters.getValue("level");
 
 		if ("*".equals(pluginName))
 			Debug.Global().setDebugLevel(level);
